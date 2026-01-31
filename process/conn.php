f36afcbf4dcde6548626e75dbe8dea854dc850ec
Heitor MBP <heitormbpereira@gmail.com>
2026-01-31 20:27:11 -0300

    Update database connection details in conn.php

--- process/conn.php
+++ process/conn.php
@@ -1,4 +1,5 @@
 <?php
-    $pdo = new PDO("mysql:host=localhost;dbname=bd_trerius","root","");
+    $pdo = new PDO("mysql:host=switchyard.proxy.rlwy.net:53660;dbname=bd_trerius","root","MGWkuAEGRgFThvcUEGfvDdpELdKCYbRJ");
 ?>
+
 <link rel="shortcut icon" href="img/Trerius.png" type="image/x-icon">
