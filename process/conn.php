b6f908f23fc382cfd8c7ca5be395eb79cca59e02
Heitor MBP <heitormbpereira@gmail.com>
2026-02-04 02:22:24 -0300

    Fix PHP opening tag formatting in conn.php

--- process/conn.php
+++ process/conn.php
@@ -2,4 +2,4 @@
     $pdo = new PDO("mysql:host=switchyard.proxy.rlwy.net:53660;dbname=bd_trerius","root","MGWkuAEGRgFThvcUEGfvDdpELdKCYbRJ");
 ?>
 
-<link rel="shortcut icon" href="img/Trerius.png" type="image/x-icon">
+
